123449e0fbce12fe54f8a0521867518f1ac5b46f
Jorge Macias <jorgemacias@hotmail.com>
2014-11-23 13:20:16 -0600

    First Commit

--- swiftCoreData/ViewController.swift
+++ swiftCoreData/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    var people = [NSManagedObject]()
+    @IBOutlet weak var labelUserName: UITextField!
+    @IBOutlet weak var labelPassword: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func loadButtonTapped(sender: UIButton) {
+        let appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        let fetchRequest = NSFetchRequest(entityName:"Users")
+        //fetchRequest.predicate = NSPredicate(format: "username= %@",labelUserName.text)
+        var error: NSError?
+        let fetchedResults = managedContext.executeFetchRequest(fetchRequest,
+            error: &error) as [NSManagedObject]?
+        if let results = fetchedResults {
+            people = results
+            println("people: \(people.count)")
+            for i in 0..<people.count {
+                let person=people[i]
+                println(person.valueForKey("username") as String?)
+                labelUserName.text=person.valueForKey("username") as String?
+                println(person.valueForKey("password") as String?)
+                labelPassword.text=person.valueForKey("password") as String?
+            }
+        } else {
+            println("Could not fetch \(error), \(error!.userInfo)")
+        }
+    }
 
+    @IBAction func saveButtonTapped(sender: UIButton) {
+        let appDelegate =
+        UIApplication.sharedApplication().delegate as AppDelegate
+        let managedContext = appDelegate.managedObjectContext!
+        let entity =  NSEntityDescription.entityForName("Users",
+            inManagedObjectContext:
+            managedContext)
+        let newUser = NSManagedObject(entity: entity!,
+            insertIntoManagedObjectContext:managedContext)
+        newUser.setValue("Test", forKey: "username")
+        newUser.setValue("1234", forKey: "password")
+        var error: NSError?
+        if !managedContext.save(&error) {
+            println("Could not save \(error), \(error?.userInfo)")
+        }
+        println(newUser)
+        println("Object Saved")
+    }
 }
 
